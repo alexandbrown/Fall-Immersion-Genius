21b7ff73426a13b85f6762a8bc0b13bcfd905dca
alexandbrown <Alexander.brown@hgs.hiddengeniusproject.org>
2024-10-22 18:00:58 -0400

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,10 +1,11 @@
 const html = document.body: function darkMode() {
 function darkMode(){
-if(html.style.backgroundColor === 'white'){
-html.style.background = 'black';
-html.style.color = 'white';
+  if(html.style.backgroundColor === 'white'){
+   html.style.background = 'black';
+   html.style.color = 'white';
 }
-else{
-  darkMode.style.background = 'white';
-  darkmode.style.color = 'black';
+  else{
+   darkMode.style.background = 'white';
+   darkmode.style.color = 'black';
+  }
 }

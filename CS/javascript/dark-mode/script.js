403ffc4b782c3fd43c224d080c8ca6002eb3647d
alexandbrown <Alexander.brown@hgs.hiddengeniusproject.org>
2024-10-22 17:57:31 -0400

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,10 @@
-function darkMode() {
-// Your code here.
+const html = document.body: function darkMode() {
+function darkMode(){
+if(html.style.backgroundColor === 'white'){
+html.style.background = 'black';
+html.style.color = 'white';
+}
+else{
+  darkMode.style.background = 'white';
+  darkmode.style.color = 'black';
 }
